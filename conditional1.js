e14ee6b9dff1b8b9f054bbbde3bf94d272a2cce5
황동현 <hwangdong@hwangdonghyeon-ui-MacBookAir.local>
2022-08-07 15:53:51 +0900

    add 자바스크립트

--- conditional1.js
+++ conditional1.js
@@ -1,9 +1,14 @@
-const age = parseInt(prompt("How Old Are You")  );
+const age = parseInt(prompt("how old are you"))
 
-if(isNaN(age)){
-    console.log("please write a number");
-} else if (age <18){
-    console.log("you are too young");
-} else {
-    console.log("you can drink");
-} 
+if (isNaN(age) || age <0 ){
+    console.log("please write a age" )
+}
+else if (age < 18){
+    console.log("보호자의 동의가 필요합니다.")
+}
+else if (age>= 18 && age<=50){
+    console.log("you can drink")
+}
+else if (age>50){
+    console.log("you are too old to drink ")
+}
---
+++ 자바 스크립트/documentObject.js
@@ -0,0 +1,31 @@
+const h1 = document.querySelector(".hello:first-child h1")
+
+function handleTitleClick(){
+    h1.style.color="blue"
+}
+function handleTitleEnter(){
+    h1.innerText = "Mouse is here"
+}
+function handleWindowResize(){
+    document.body.style.backgroundColor="tomato"
+}
+function handleWindowCopy(){
+    alert("copier!")
+}
+function handleWindowOffline(){
+    alert("인터넷 연결 안됨")
+}
+function handleWindowOnline(){
+    alert("인터넷 연결 완료")
+}
+
+
+
+h1.onclick=handleTitleClick
+h1.addEventListener("mouseenter",handleTitleEnter)
+
+
+window.addEventListener("resize", handleWindowResize)
+window.addEventListener("copy",handleWindowCopy)
+window.addEventListener("offline", handleWindowOffline)
+window.addEventListener("online", handleWindowOnline)
