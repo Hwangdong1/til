43636bc763cd205e972db3b0ce755b96d8e5196b
Hwangdong1 <ehdgus0315@naver.com>
2022-08-10 22:59:44 +0900

    ㅇㅇ

--- 자바 스크립트/documentObject.js
+++ 자바 스크립트/documentObject.js
@@ -1,31 +1,15 @@
 const h1 = document.querySelector(".hello:first-child h1")
 
 function handleTitleClick(){
-    h1.style.color="blue"
-}
-function handleTitleEnter(){
-    h1.innerText = "Mouse is here"
-}
-function handleWindowResize(){
-    document.body.style.backgroundColor="tomato"
-}
-function handleWindowCopy(){
-    alert("copier!")
-}
-function handleWindowOffline(){
-    alert("인터넷 연결 안됨")
-}
-function handleWindowOnline(){
-    alert("인터넷 연결  어쩌라고 황길성 멍청이 제발좀 되라")
+    const curretColor =  h1.style.color
+    let newColor
+    if(curretColor === "blue"){
+        newColor = "tomato"
+    } else {
+    newColor = "blue"
+    }
+    h1.style.color = newColor
 }
 
 
-
-h1.onclick=handleTitleClick
-h1.addEventListener("mouseenter",handleTitleEnter)
-
-
-window.addEventListener("resize", handleWindowResize)
-window.addEventListener("copy",handleWindowCopy)
-window.addEventListener("offline", handleWindowOffline)
-window.addEventListener("online", handleWindowOnline)
+h1.addEventListener("click",handleTitleClick)
