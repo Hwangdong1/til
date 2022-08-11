23b3ccbb7feea8bb8daa73a80f3fa672e4c97e37
Hwangdong1 <ehdgus0315@naver.com>
2022-08-12 00:23:37 +0900

    toggle

--- 자바 스크립트/documentObject.js
+++ 자바 스크립트/documentObject.js
@@ -1,14 +1,8 @@
 const h1 = document.querySelector(".hello:first-child h1")
 
 function handleTitleClick(){
-    const curretColor =  h1.style.color
-    let newColor
-    if(curretColor === "blue"){
-        newColor = "tomato"
-    } else {
-    newColor = "blue"
-    }
-    h1.style.color = newColor
+    const clickedClass ="clicked"
+    h1.classList.toggle(clickedClass)
 }
 
 
