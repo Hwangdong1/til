d3e1047c841d873f2320ee8327f1f4866509acb8
Hwangdong1 <ehdgus0315@naver.com>
2022-08-13 23:52:31 +0900

    login

--- 자바 스크립트/documentObject.js
+++ 자바 스크립트/documentObject.js
@@ -1,9 +1,26 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
+const greeting = document.querySelector("#greeting")
+
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "userName"
 
 function onLoginSubmit (event){
     event.preventDefault()
     const userName = loginInput.value
-    console.log(userName)
+    loginForm.classList.add("hidden")
+    console.log(USERNAME_KEY)
+    localStorage.setItem("userName", USERNAME_KEY)
+    greeting.innerText = "Hello " + USERNAME_KEY
+    greeting.classList.remove("hidden")
 }
-loginForm.addEventListener("submit", onLoginSubmit)
+
+loginForm.addEventListener("submit", onLoginSubmit) 
+
+const saveUserName = localStorage.getItem(USERNAME_KEY)
+
+if (saveUserName === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME)
+} else {
+    greeting.classList.remove(HIDDEN_CLASSNAME)
+}
