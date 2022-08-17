7bf297a556db0d01257d5ad9eab45a2f3b260100
Hwangdong1 <ehdgus0315@naver.com>
2022-08-17 23:26:42 +0900

    hi

--- 자바 스크립트/documentObject.js
+++ 자바 스크립트/documentObject.js
@@ -24,6 +24,7 @@ if (saveUserName === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  greeting.innerText = "Hello!! " + saveUserName;
+  greeting.innerText =
+    "Hello!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! " + saveUserName;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
